2398d03e615cceffe6727489a90f3c471685e921
Aline Stefanie Dias Lage <70866061+AlineLage@users.noreply.github.com>
2021-01-07 19:24:30 -0300

    Estrutura condicional 6
    Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem 
    dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) 
    este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos,
    deverá ser impressa a mensagem “Fora de intervalo”.

---
+++ EstruturaCondicional6_.java
@@ -0,0 +1,45 @@
+/*
+Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem 
+dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) 
+este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos,
+deverá ser impressa a mensagem “Fora de intervalo”.
+ */
+package estruturacondicional6_;
+import java.util.Scanner;
+
+/**
+ *
+ * @author SAMSUNG
+ */
+public class EstruturaCondicional6_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double numero, intervalo;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Informe um número: ");
+        numero = entrada.nextDouble();
+        
+        if (numero >= 0 && numero <= 25) {
+            System.out.println("O número informado está entre o intervalo [0, 25]");
+        }
+        else if (numero > 25 && numero <= 50) {
+            System.out.println("O número informado está entre o intervalo [25, 50]");
+        }
+        else if (numero > 50 && numero <= 75) {
+            System.out.println("O número informado está entre o intervalo [50, 75]");
+        }    
+        else if (numero > 75 && numero <= 100) {
+            System.out.println("O número informado está entre o intervalo [75, 100]");
+        } 
+        else{
+            System.out.println("O número informado está fora do intervalo!");
+        }
+    }
+    
+}
